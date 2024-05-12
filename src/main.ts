4444e04927e6037a88574bda73ae5d91b8557d35
tarnoposkyi <tarnopolskyid@gmail.com>
2024-05-12 17:15:34 +0200

    generate basic resources

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // set global prefix for all routes - http://localhost:3000/api
+  app.setGlobalPrefix('api');
+  // enable Cross-Origin Resource Sharing
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
