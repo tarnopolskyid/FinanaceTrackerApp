d4a4282b7c9d652ac40cb313705c8507179226a9
tarnoposkyi <tarnopolskyid@gmail.com>
2024-06-02 18:47:47 +0200

    create relations between entities

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,35 @@
-export class Category {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne, OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { User } from '../../user/entities/user.entity'
+import { Transaction } from '../../transaction/entities/transaction.entity'
+
+@Entity()
+export class Category {
+
+  @PrimaryGeneratedColumn({name: 'categoryId'})
+  id: number;
+
+  @Column()
+  title: string;
+
+  @ManyToOne(() => User, (user) => user.categories)
+  @JoinColumn({name: 'user_id'})
+  user: User;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.category)
+  transactions: Transaction[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+}
--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,41 @@
-export class Transaction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { User } from '../../user/entities/user.entity'
+import { Category } from '../../category/entities/category.entity'
+
+@Entity()
+export class Transaction {
+
+  @PrimaryGeneratedColumn({ name: 'transactionId'})
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  amount: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({nullable: true})
+  type: string;
+
+  @ManyToOne(() => User, (user) => user.transactions)
+  @JoinColumn({name: 'user_id'})
+  user: User;
+
+  @ManyToOne(() => Category, (category) => category.transactions)
+  @JoinColumn({name: 'category_id'})
+  category: Category;
+}
