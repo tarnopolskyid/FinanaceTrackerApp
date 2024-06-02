77d0af01a9f7297e412445895749e39f00f0fc23
tarnoposkyi <tarnopolskyid@gmail.com>
2024-06-02 20:09:30 +0200

    prepare backend for user registration

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsEmail, MinLength } from 'class-validator'
+
+export class CreateUserDto {
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6, {message: 'Password is too short, at least 6 characters, please'})
+  password: string;
+}
