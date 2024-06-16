d3c40062cbc3b3d2e38a885ec1653d84a31f1b84
tarnoposkyi <tarnopolskyid@gmail.com>
2024-06-16 20:18:14 +0200

    authorization and registration - jwt token used

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module'
+import { PassportModule } from '@nestjs/passport'
+import { LocalStrategy } from './strategies/local.strategies'
+import { JwtModule } from '@nestjs/jwt'
+import { environments } from 'eslint-plugin-prettier'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {expiresIn: '30d'}
+      }),
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
