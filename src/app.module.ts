22cf979e38c6e95549a1b999aff0d09f8a00cbbc
tarnoposkyi <tarnopolskyid@gmail.com>
2024-11-27 12:15:07 +0100

    fix for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,8 @@ import {TypeOrmModule} from "@nestjs/typeorm";
             imports: [ConfigModule],
             useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
-                host: configService.get('DB_HOST'),
-                port: configService.get('DB_PORT'),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_NAME'),
+                url: process.env.DATABASE_URL, // Získám z Railway
+                autoLoadEntities: true,
                 synchronize: true,
                 entities: [__dirname + '/**/*.entity{.js, .ts}']
             }),
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   // enable Cross-Origin Resource Sharing
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
