b8b0547ae144b38cf0e3acbf3fdbbd57b18d9e73
tarnoposkyi <tarnopolskyid@gmail.com>
2024-11-27 14:10:21 +0100

    fix for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,17 +15,20 @@ import {TypeOrmModule} from "@nestjs/typeorm";
         AuthModule,
         TransactionModule,
         ConfigModule.forRoot({isGlobal: true}),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            url: process.env.DATABASE_URL, // Získám z Railway
-
-            // host: configService.get('DB_HOST'),
-            // port: configService.get('DB_PORT'),
-            // username: configService.get('DB_USERNAME'),
-            // password: configService.get('DB_PASSWORD'),
-            // database: configService.get('DB_NAME'),
-            synchronize: true,
-            entities: [__dirname + '/**/*.entity{.js, .ts}']
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                url: process.env.DATABASE_URL, // Získám z Railway
+                // host: configService.get('DB_HOST'),
+                // port: configService.get('DB_PORT'),
+                // username: configService.get('DB_USERNAME'),
+                // password: configService.get('DB_PASSWORD'),
+                // database: configService.get('DB_NAME'),
+                synchronize: true,
+                entities: [__dirname + '/**/*.entity{.js, .ts}']
+            }),
+            inject: [ConfigService]
         })
     ],
     controllers: [AppController],
