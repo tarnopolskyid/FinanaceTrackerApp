783f942a96561d1bf3f8bbf217f21e6491605565
tarnoposkyi <tarnopolskyid@gmail.com>
2024-11-27 16:46:38 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // set global prefix for all routes - http://localhost:3000/api
   app.setGlobalPrefix('api');
-  // enable Cross-Origin Resource Sharing
-  app.enableCors();
+  // Nastavení CORS
+  app.enableCors({
+    origin: 'https://finanace-tracker-app-frontend.vercel.app', // Povolit požadavky z této domény
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Povolené HTTP metody
+    allowedHeaders: ['Content-Type', 'Authorization'], // Povolené hlavičky
+    credentials: true, // Pokud chcete povolit cookies nebo jiné credentials
+  });
   await app.listen(3000);
 }
 bootstrap();
